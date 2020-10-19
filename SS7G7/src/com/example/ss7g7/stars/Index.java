c2e01a852a0303f3fcc5ff3bc441307f15b90331
shah <h.jshah39@gmail.com>
2020-10-19 10:54:02 +0800

    Created Course & Index classes

---
+++ SS7G7/src/com/example/ss7g7/stars/Index.java
@@ -0,0 +1,13 @@
+package com.example.ss7g7.stars;
+import java.util.ArrayList;
+
+public class Index {
+	private int indexNum;
+	private int numVacancy;
+	
+	public Index(int index_Num, int num_Vacancy) {
+		this.indexNum=index_Num;
+		this.numVacancy=num_Vacancy;
+	}
+
+}
