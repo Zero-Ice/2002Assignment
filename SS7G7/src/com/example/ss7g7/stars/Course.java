b9647889d3642253bc394bf46711c4120072326b
shah <h.jshah39@gmail.com>
2020-10-19 15:20:10 +0800

    Added Lab, Tut, Lec for Index

--- SS7G7/src/com/example/ss7g7/stars/Course.java
+++ SS7G7/src/com/example/ss7g7/stars/Course.java
@@ -1,14 +1,27 @@
 package com.example.ss7g7.stars;
 
+import java.util.ArrayList;
+
 public class Course {
 	private String courseCode;
 	private String courseName;
+	private ArrayList<Index> indexes; // array of indexes
+	private boolean courseAvailability;
 	
 	public Course(String course_Code, String course_Name) {
-		this.setCourseCode(course_Code);
-		this.setCourseName(course_Name);
+		this.courseCode=course_Code;
+		this.courseName=course_Name;
+		indexes = new ArrayList<Index>();
+	}
+	
+	public void addIndex(int index, int vacancy) {
+		indexes.add(new Index(index,vacancy));
+	}
+	
+	public void removeIndex(int index) {
 		
 	}
+	
 
 	public String getCourseCode() {
 		return courseCode;
