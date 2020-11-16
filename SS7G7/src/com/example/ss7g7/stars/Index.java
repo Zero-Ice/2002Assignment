33b229ee9f54ed6d200c13dbc3e36b845ad75e05
shah <h.jshah39@gmail.com>
2020-11-16 17:30:04 +0800

    Documentation of Index Class

--- SS7G7/src/com/example/ss7g7/stars/Index.java
+++ SS7G7/src/com/example/ss7g7/stars/Index.java
@@ -10,10 +10,21 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+/**
+ * <h1>Index Class</h1> The Index class is a subclass of Course class. It stores the
+ * details of a the index for the course.
+ * 
+ * <p>
+ *  This class is used to determine the index details. For each course the number of indexes
+ *  may vary. Therefore the index class will be a subclass for the course class.
+ * 
+ *
+ * @author Jabir Shah Halith
+ * @since 2020-10-16
+ */
+
+
 public class Index implements Serializable{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -464339125814698090L;
 	
 	private int indexNum;
@@ -37,10 +48,13 @@ public class Index implements Serializable{
 	private SendEmail send;
 	
 	//TODO: email body and subject
-	//TODO: return waitlist
 	
-	
-	// Constructor for Index Class
+	/**
+	 * Constructor for Index Class
+	 * @param index_Num - the index number of the index
+	 * @param courseCode - the course code the index belongs to
+	 * @param num_Seat - the number of seats available for the index class
+	 */
 	public Index(int index_Num, String courseCode, int num_Seat) {
 		this.indexNum=index_Num;
 		this.courseCode = courseCode;
@@ -62,7 +76,11 @@ public class Index implements Serializable{
 		}
 	}
 	
-	// Method return boolean on whether student has registered to current index
+	/**
+	 * Method return boolean on whether student has registered to current index
+	 * @param matricNo - the matric number of the student
+	 * @return True - if student has been registered in the course before <br/> False - if student has not been registered in the course before
+	 */
 	public boolean isStudentRegistered(String matricNo) {
 		if(seatVacancy.contains(matricNo)) {
 			return true;
@@ -71,7 +89,9 @@ public class Index implements Serializable{
 		}
 	}
 
-	// Method to print students registered for the index
+	/**
+	 *  Method to print students registered in the index
+	 */
 	public void printStudentListByIndex() {
 		System.out.println(indexNum);
 		for(int i =0;i<numSeats;i++) {
@@ -82,6 +102,9 @@ public class Index implements Serializable{
 		System.out.println();
 	}
 	
+	/**
+	 * 
+	 */
 	@Override
 	public String toString() {
 		// Example
@@ -94,23 +117,35 @@ public class Index implements Serializable{
 	}
 	
 ///////////////////////    Index Related          ////////////////////////////
-	
-	// getter for index number
+
+	/**
+	 * Accessor method to get the index number
+	 * @return index number
+	 */
 	public int getIndexNum() {
 		return indexNum;
 	}
 
-	// setter for index number ( to update index number)
+	/**
+	 * Mutator method to change the index number
+	 * @param indexNum - intended new index number
+	 */
 	public void setIndexNum(int indexNum) {
 		this.indexNum = indexNum;
 	}
 
-	// returns the vacancy list 
+	/**
+	 * Method to return the seat vacancy list
+	 * @return the vacancy list 
+	 */
 	public ArrayList<String> getVacancyList(){
 		return seatVacancy;
 	}
 	
-	// return number of vacancies left
+	/**
+	 * Method to return the number of vacancies left in an index
+	 * @return the number of vacancies left in the index
+	 */
 	public int getNumOfVacancies() {
 		int numOfVacant=0;
 		
@@ -124,6 +159,10 @@ public class Index implements Serializable{
 	
 /************************************************************************************************************/
 ///////////////////////                    Assign and Unassign Student from index          ////////////////////////////
+	/**
+	 * Method to assign student to an index
+	 * @param student - the student object to be added to the index
+	 */
 	public void assignStudent(Student student) {
 		String matricNo = student.getMatricNo();
 		
@@ -148,6 +187,12 @@ public class Index implements Serializable{
 		student.addWaitingCourse(this.courseCode, this.indexNum);
 	}
 	
+	/**
+	 * Method to unassign a student from the index
+	 * @param student - the student object to be unassigned from the index
+	 * @param triggerWaitlistUpdate
+	 * @return the student object
+	 */
 	public Student unassignStudent(Student student, boolean triggerWaitlistUpdate) {
 		String matricNo = student.getMatricNo();
 		
@@ -187,7 +232,10 @@ public class Index implements Serializable{
 
 ///////////////////////                    Waitlist        ////////////////////////////
 	
-	// Method to add student to waitlist
+	/**
+	 * Method to add student to the waitlist
+	 * @param student - the student to be added to the waiting list
+	 */
 	public void addStudentToWaitlist(Student student) {
 
 		if(isStudentInWaitlist(student)==false) {
@@ -202,7 +250,9 @@ public class Index implements Serializable{
 		
 	}
 	
-	// Method to add student from waitlist into index
+	/**
+	 * Method to add student from waitlist into index when there is an available slot
+	 */
 	public void addFromWaitlistToIndex() {
 		if(studentWaitlist.size()==0) {
 			System.out.println("no student in waitlist");
@@ -213,7 +263,9 @@ public class Index implements Serializable{
 		}
 	}
 	
-	// Shows the students who are in waitlist
+	/**
+	 * Method to show the students who are in the waitlist
+	 */
 	public void showStudentWaitlist() {
 		if(studentWaitlist.size()==0) {
 			System.out.println("no student in waitlist");
@@ -224,7 +276,11 @@ public class Index implements Serializable{
 		}
 	}
 	
-	// checks if student has already been placed on waitlist
+	/**
+	 * Method that checks if student has already been placed on waitlist
+	 * @param student
+	 * @return True - if student is in waitlist <br/> False - if student is not in the waitlist
+	 */
 	public boolean isStudentInWaitlist(Student student) {
 		String matricNo = student.getMatricNo();
 		for(int i =0;i<studentWaitlist.size();i++) {
@@ -235,11 +291,34 @@ public class Index implements Serializable{
 		}
 		return false;
 	}
+	
+	
+	/**
+	 * Method to return the amount of students in waitlist
+	 * @return number of student in waitlist
+	 */
+	public int getWaitlistLength() {
+		return this.studentWaitlist.size();
+	}
 
 /************************************************************************************************************/
 
 
 ///////////////////////                    SET tut details              ////////////////////////////
+	
+	/**
+	 * Method to set Tutorial details such as the time,venue, group and remark at once
+	 * 
+	 * @param intDay - the integer of the day of tutorial
+	 * @param startHours - the time(hour) the tutorial starts
+	 * @param startMinutes  - the time(minutes) the tutorial starts
+	 * @param endHours - the time(hour) the tutorial ends
+	 * @param endMinutes - the time(minutes) the tutorial ends
+	 * @param tutVenue - the venue of the tutorial
+	 * @param tutRemarks - remarks of the tutorial
+	 * @param tutGroup - the group the tutorial belongs to
+	 * @param occurring
+	 */
 	public void setTutDetails(int intDay, int startHours, int startMinutes,int endHours,
 			int endMinutes,String tutVenue, String tutRemarks, String tutGroup, int occurring) {
 		tutStartTime = LocalDateTime.of(2020, Month.JUNE, intDay, startHours, startMinutes);
@@ -254,15 +333,26 @@ public class Index implements Serializable{
 /************************************************************************************************************/
 	
 ///////////////////////                    edit tut Details (setters)            ////////////////////////////
-	
+	/**
+	 * Mutator method to change the venue for tutorial
+	 * @param tutVenue - intended new venue for tutorial
+	 */
 	public void updateTutVenue(String tutVenue) {
 		this.tutVenue = tutVenue;
 	}
 
+	/**
+	 * Mutator method to change the remark for tutorial
+	 * @param tutRemark - intended new remark for tutorial
+	 */
 	public void updateTutRemark(String tutRemark) {
 		this.tutRemark = tutRemark;
 	}
 	
+	/**
+	 * Mutator method to change the group for tutorial
+	 * @param tutGroup - intended new group for tutorial
+	 */
 	public void updateTutGroup(String tutGroup) {
 		this.tutGroup = tutGroup;
 	}
@@ -271,6 +361,10 @@ public class Index implements Serializable{
 
 ///////////////////////                    get tut Details (getters)            ////////////////////////////
 	
+	/**
+	 * Method to get all the tutorial details such as Day, Time, Group, Venue and Remark  at once 
+	 * @return the full details of the tutorial
+	 */
 	public String getTutDetails() {
 		if(tutStartTime == null || tutEndTime == null) return "";
 		
@@ -286,26 +380,50 @@ public class Index implements Serializable{
 		return tutDetails;
 	}
 	
+	/**
+	 * Accessor method to the get venue of tutorial
+	 * @return venue of tutorial
+	 */
 	public String getTutVenue() {
 		return tutVenue;
 	}
 	
+	/**
+	 * Accessor method to the get remark of tutorial
+	 * @return remark of tutorial
+	 */
 	public String getTutRemark() {
 		return tutRemark;
 	}
 	
+	/**
+	 * Accessor method to the get tutorial group 
+	 * @return tutorial group 
+	 */
 	public String getTutGroup() {
 		return tutGroup;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getTutOccurring() {
 		return tutOccurring;
 	}
 	
+	/**
+	 * Accessor method to the get starting time of tutorial
+	 * @return starting time of tutorial
+	 */
 	public LocalDateTime getTutStartTime() {
 		return tutStartTime;
 	}
 	
+	/**
+	 * Accessor method to the get ending time of tutorial
+	 * @return ending time of tutorial
+	 */
 	public LocalDateTime getTutEndTime() {
 		return tutEndTime;
 	}
@@ -313,7 +431,19 @@ public class Index implements Serializable{
 /************************************************************************************************************/
 
 ///////////////////////                    SET lab details              ////////////////////////////
-	
+	/**
+	 * Method to set Lab details such as the time,venue, group and remark at once
+	 * 
+	 * @param intDay - the integer of the day of Lab
+	 * @param startHours - the time(hour) the Lab starts
+	 * @param startMinutes - the time(minutes) the Lab starts
+	 * @param endHours - the time(hour) the Lab ends
+	 * @param endMinutes - the time(minutes) the Lab ends
+	 * @param labVenue - the venue of the Lab
+	 * @param labRemarks - remarks of the Lab
+	 * @param labGroup - the group the Lab belongs to
+	 * @param occurring
+	 */
 	public void setLabDetails(int intDay, int startHours, int startMinutes,int endHours, int endMinutes,
 			String labVenue,String labRemarks, String labGroup, int occurring) {
 		
@@ -330,14 +460,26 @@ public class Index implements Serializable{
 	
 ///////////////////////                    edit lab Details (setters)            ////////////////////////////
 	
+	/**
+	 * Mutator method to change the venue for lab
+	 * @param labVenue - intended new venue for lab
+	 */
 	public void updateLabVenue(String labVenue) {
 		this.labVenue = labVenue;
 	}
 
+	/**
+	 * Mutator method to change the group for lab
+	 * @param labGroup - intended new group for lab
+	 */
 	public void updateLabGroup(String labGroup) {
 		this.labGroup = labGroup;
 	}
 	
+	/**
+	 * Mutator method to change the remark for lab
+	 * @param labRemark - intended new remark for lab
+	 */
 	public void updateLabRemark(String labRemark) {
 		this.labRemark = labRemark;
 	}
@@ -345,7 +487,10 @@ public class Index implements Serializable{
 /************************************************************************************************************/
 	
 ///////////////////////                    get lab Details (getters)            ////////////////////////////
-	
+	/**
+	 * Method to get all the lab details such as Day, Time, Group, Venue and Remark  at once 
+	 * @return the full details of the lab
+	 */
 	public String getLabDetails() {
 		if(labStartTime == null || labEndTime == null) return "";
 		
@@ -359,34 +504,54 @@ public class Index implements Serializable{
 		return labDetails;
 	}
 	
+	/**
+	 * Accessor method to the get lab venue 
+	 * @return lab venue 
+	 */
 	public String getLabVenue() {
 		return labVenue;
 	}
 	
-	
+	/**
+	 * Accessor method to the get lab group 
+	 * @return lab group 
+	 */
 	public String getLabGroup() {
 		return labGroup;
 	}
 	
+	/**
+	 * Accessor method to the get lab remark 
+	 * @return lab remark 
+	 */
 	public String getLabRemark() {
 		return labRemark;
 	}
 	
+	/**
+	 * Accessor method to the get starting time of lab
+	 * @return starting time of lab
+	 */
 	public LocalDateTime getLabStartTime() {
 		return labStartTime;
 	}
 	
+	/**
+	 * Accessor method to the get ending time of lab 
+	 * @return ending time of lab 
+	 */
 	public LocalDateTime getLabEndTime() {
 		return labEndTime;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getLabOccurring() {
 		return labOccurring;
 	}
-	
-	public int getWaitlistLength() {
-		return this.studentWaitlist.size();
-	}
+
 	
 /************************************************************************************************************/
 	
